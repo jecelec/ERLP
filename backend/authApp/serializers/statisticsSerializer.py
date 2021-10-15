c0c2054993f3f259acbc7b16d3f686af58423ebc
jecelec <75706186+jecelec@users.noreply.github.com>
2021-10-15 00:08:59 -0500

    Serializers, Views, Urls
    Configuraciones de serializers, views, urls

--- backend/authApp/serializers/statisticsSerializer.py
+++ backend/authApp/serializers/statisticsSerializer.py
@@ -1,20 +1,33 @@
 from authApp.models.statistics import historico
-from authApp.models.user    import User
-from rest_framework                import serializers
+from authApp.models.user       import User
+from rest_framework            import serializers
 
-class AccountSerializer(serializers.ModelSerializer):
+class StatisticsSerializer(serializers.ModelSerializer):
     class Meta:
         model  = historico
-        fields = ['balance', 'lastChangeDate', 'isActive']
+        fields = ['id', 'departamento', 'municipio', 'codigo_arl', 'year', 
+        'month', 'economia', 'rela_dep', 'rela_indep', 'presu_acci_rasu', 'muertes_AT', 
+        'pensiones_AT', 'pensiones_EL', 'incapacidades_EL', 'incapacidades_AT']
 
     def to_representation(self, obj):
         user    = User.objects.get(id=obj.user)
         estadistica = historico.objects.get(id=obj.id)
         return {
-            'id'             : historico.id,
-            'balance'        : historico.balance,
-            'lastChangeDate' : historico.lastChangeDate,
-            'isActive'       : historico.isActive,
+            'id'              : historico.id,
+            'departamento'    : historico.departamento,
+            'municipio'       : historico.municipio,
+            'codigo_arl'      : historico.codigo_arl,
+            'year'            : historico.year,
+            'month'           : historico.month,
+            'economia'        : historico.economia,
+            'rela_dep'        : historico.rela_dep,
+            'rela_indep'      : historico.rela_indep,
+            'presu_acci_rasu' : historico.presu_acci_rasu,
+            'muertes_AT'      : historico.muertes_AT,
+            'pensiones_AT'    : historico.pensiones_AT,
+            'pensiones_EL'    : historico.pensiones_EL,
+            'incapacidades_EL': historico.incapacidades_EL,
+            'incapacidades_AT': historico.incapacidades_AT,
             'user' : {
                 'id'    : user.id,
                 'name'  : user.name,
